0ac5fbdea3656cadcae4f76ecaad23e85dd27d13
Danis Zaidan <daniszaidan@gmail.com>
2025-06-18 21:25:19 +0700

    trying fix http not secure

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://202.157.176.100:3001/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
